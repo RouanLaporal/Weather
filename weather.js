8d140560745ccf02602ee4b2a26d8b8385e30bf7
rouan <rouan.laporal@outlook.com>
2021-09-28 15:20:19 +0200

    réécriture de la fonction getDayName et finition de la weekList

--- weather.js
+++ weather.js
@@ -1,4 +1,5 @@
 /*WeekList a terminer, problème à résoudre sur la fonction pour le jour de la semaine getDay retourne uniquement 1*/
+var date = new Date();
 let dateDay = document.getElementById("dateDay");
 let dateDayName = document.getElementById("dateDayName");
 let temp = document.getElementById("tempC");
@@ -9,10 +10,13 @@ let weatherDesc = document.getElementById("weatherDesc");
 let pressionValue = document.getElementById("pressionValue");
 let loc = document.getElementById("location");
 let weekDayName = document.getElementById("weekDayName");
+let weekTemp = document.getElementById("weekTemp");
 fetch("http://api.weatherapi.com/v1/current.json?key=7a5e39162d0044c8972113559212709&q=Bagnolet&aqi=yes")
 .then(response => response.json())
 .then(response =>{
-    dateDayName.innerHTML = getDayName(response.location.localtime);
+    dateDayName.innerHTML = getDayName(date.now, 'long');
+    weekDayName.innerHTML = getDayName(date.now, 'short');
+    weekTemp.innerHTML = response.current.temp_c + " °C";
     loc.innerHTML = response.location.name + ", " + response.location.region;
     dateDay.innerHTML = response.location.localtime;
     temp.innerHTML = response.current.temp_c + " °C";
@@ -25,41 +29,26 @@ fetch("http://api.weatherapi.com/v1/current.json?key=7a5e39162d0044c897211355921
 fetch("http://api.weatherapi.com/v1/forecast.json?key=7a5e39162d0044c8972113559212709&q=Bagnolet&days=7&aqi=no&alerts=yes")
 .then(response => response.json())
 .then(response =>{
-    console.log(response.forecast.forecastday[1].date);
-    console.log(getDayName(response.forecast.forecastday[1].date));
-    for(let i =0; i<2;i++){
-        console.log(getDayName(response.forecast.forecastday[i].date).substring(0,3));
+    for(var i=1;i<3;i++){
+        var newLi = document.createElement('li');
+        var firstspan = document.createElement('span');
+        var secspan = document.createElement('span');
+        var day = date.getDate() + 1;
+        newLi.className = 'active';
+        newLi.id = "active" + i;
+        firstspan.className = 'day-name';
+        firstspan.innerHTML = getDayName(date.setDate(day), 'short');
+        firstspan.id = "weekDayName"+i;
+        secspan.className = 'day-temp';
+        secspan.innerHTML = response.forecast.forecastday[i].day.maxtemp_c + " °C";
+        document.getElementById('weekList').appendChild(newLi);
+        document.getElementById('active'+i).appendChild(firstspan);
+        document.getElementById('weekDayName' + i).appendChild(secspan);
     }
     
 })
-function getDayName(localtime){
-    let date = new Date();
-    let name;
-    console.log("localtime = "+localtime);
-    let dateD = date.getDay(localtime);
-    console.log("dateD = " + dateD);
-    switch(dateD){
-        case 0:
-            name = "Sunday";
-            break;
-        case 1:
-            name = "Monday";
-            break;
-        case 2:
-            name = "Tuesday";
-            break;
-        case 3:
-            name = "Wednesday";
-            break;
-        case 4:
-            name = "Thursday";
-            break;
-        case 5:
-            name = "Friday";
-            break;
-        default:
-           name = "Saturday";
-            
-    }
-    return name;
+function getDayName(localtime,string){
+    var options = { weekday: string };
+    console.log("function =" + date.getDay(localtime));
+    return new Intl.DateTimeFormat('en-US', options).format(date);
 }
